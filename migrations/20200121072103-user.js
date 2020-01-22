2c34ff58c7b88ca7cd02de5c4dbf139aa0c8ac24
Chaim Finkelman <toChaim@gmail.com>
2020-01-21 17:10:10 -0800

    first two tables

--- migrations/20200121072103-user.js
+++ migrations/20200121072103-user.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var dbm;
 var type;
@@ -8,39 +8,104 @@ var seed;
  * We receive the dbmigrate dependency from dbmigrate initially.
  * This enables us to not have to rely on NODE_PATH.
  */
-exports.setup = function(options, seedLink) {
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db, callback) {
+exports.up = function (db, callback) {
   db.createTable(
-    "user",
+    'user',
     {
       id: {
-        type: "int",
-        primaryKey: true
+        type: 'int',
+        primaryKey: true,
+        autoIncrement: true,
       },
       user_name: {
-        type: "string",
-        length: 40
+        type: 'string',
+        length: 40,
+        unique: true,
       },
       email: {
-        type: "string",
-        length: 50
+        type: 'string',
+        unique: true,
+      },
+      password: {
+        type: 'char',
+        length: 128,
+        notNull: true,
       }
     },
-    function(err) {
+    function (err) {
       if (err) return callback(err);
       return callback();
     }
   );
+
+  db.createTable(
+    'pledge',
+    {
+      id: {
+        type: 'int',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      creation_date: {
+        type: 'timestamp',
+        notNull: true,
+        defaultValue: new String('CURRENT_TIMESTAMP'),
+      },
+      from: {
+        type: 'int',
+        notNull: true,
+        foreignKey: {
+          name: 'from_user',
+          table: 'user',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      },
+      to: {
+        type: 'int',
+        foreignKey: {
+          name: 'to_user',
+          table: 'user',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        },
+      },
+      amount: {
+        type: 'int',
+        notNull: true,
+      },
+      for: {
+        type: 'string',
+        notNull: true,
+      },
+      resolve_date: {
+        type: 'timestamp',
+      },
+      resolved: {
+        type: 'boolean'
+      }
+    },
+    err => { if (err) { return callback(err); } return callback(); }
+  );
+
   return null;
 };
 
-exports.down = function(db, callback) {
-  db.dropTable("user", callback);
+exports.down = function (db, callback) {
+  db.dropTable('pledge', callback);
+  db.dropTable('user', callback);
   return null;
 };
 
